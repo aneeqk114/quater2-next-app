06cb2b863444402ef38695b5ef1c58065f6851a8
aneeqk114 <140748008+aneeqk114@users.noreply.github.com>
2024-01-19 23:11:48 +0500

    Add files via upload

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return ( 
+    <div>
+    <h1 className="text-3xl font-bold bg-blue-700 text-center">Hello World!</h1>
+  </div>
+  )
+}
